05fbc8fd88c51e3a669111700a6e4aafaffb2a0d
Marcos <marcosmafeimachado@gmail.com>
2022-05-04 20:50:15 -0300

    feat: added the enviroment configuration to the next config file

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains:['images.stockx.com']
+  },
+  env:{
+    API_ENDPOINT: process.env.API_ENDPOINT,
+    FIREBASE_KEY: process.env.FIREBASE_KEY,
+    FIREBASE_AUTH: process.env.FIREBASE_AUTH,
+    FIREBASE_ID: process.env.FIREBASE_ID,
+    FIREBASE_BUCKET: process.env.FIREBASE_BUCKET,
+    FIREBASE_SENDER: process.env.FIREBASE_SENDER,
+    FIREBASE_APP: process.env.FIREBASE_APP,
+    FIREBASE_MEASUREMENT: process.env.FIREBASE_MEASUREMENT,
   }
 }
 
